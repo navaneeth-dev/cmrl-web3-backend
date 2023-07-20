2295ca2416367e86ac262f487d4afcec2973e621
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-20 10:01:19 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,72 @@
-import express, { Request, Response } from "express";
-const app = express();
-const port = 3000;
+import Fastify from "fastify";
+import { ConnectionOptions, Queue, Worker } from "bullmq";
+import "dotenv/config";
+import { envSchema } from "./models/env.schema";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+// To fix
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+};
+const fastify = Fastify({
+  logger: envToLogger.development,
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const main = async () => {
+  const env = envSchema.safeParse(process.env);
+  if (!env.success) {
+    fastify.log.error(
+      { errors: env.error.errors },
+      "Environment variables not set."
+    );
+    return;
+  }
+
+  const connection: ConnectionOptions = {
+    host: env.data.REDISHOST,
+    password: env.data.REDISPASSWORD,
+    username: env.data.REDISUSER,
+    port: env.data.REDISPORT,
+  };
+
+  const worker = new Worker(
+    "foo",
+    async (job) => {
+      console.log("called");
+      await new Promise((r) => setTimeout(r, 3000));
+      console.log(job.data);
+    },
+    {
+      connection,
+    }
+  );
+
+  const myQueue = new Queue("foo", {
+    connection,
+  });
+
+  fastify.get("/", async (req, res) => {
+    await myQueue.add("myJobName", { foo: "bar" });
+    return "Hello";
+  });
+
+  try {
+    const server = await fastify.listen({ port: 3000 });
+    fastify.log.info(server, "hello");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, async () => {
+    await fastify.close();
+    process.exit(0);
+  });
 });
+
+main();
---
+++ src/models/env.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+  REDISHOST: z.string(),
+  REDISUSER: z.string(),
+  REDISPASSWORD: z.string(),
+  REDISPORT: z.preprocess(
+    (port) => parseInt(z.string().parse(port), 10),
+    z.number().positive().max(65535)
+  ),
+  NODE_ENV: z.string().default("development"),
+});
