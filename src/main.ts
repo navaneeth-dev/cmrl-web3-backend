43c283350b5b0d5b9fc10f7093986651c5d8d750
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-23 09:47:43 +0530

    no need react support

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { pinoHttp } from "pino-http";
 import logger from "./utils/logger";
 import routes from "./routes";
-import ENV from "./schemas/env";
 
 const app = express();
 
