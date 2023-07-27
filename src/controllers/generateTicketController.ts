a79a0c66a10735edfa70040d4c851415cb690ef9
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-27 17:28:23 +0530

    boilerplate

--- src/controllers/generateTicketController.ts
+++ src/controllers/generateTicketController.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import { receiver } from "../../config/qstashConfig";
 
-const generateTicketController = async (_req: Request, res: Response) => {
+const generateTicketController = async (req: Request, res: Response) => {
   // Verify from QStash
+  const { body } = req;
   const isValid = await receiver.verify({
-    signature: request.headers.get("Upstash-Signature") ?? "",
+    signature: req.get("Upstash-Signature") ?? "",
     body,
   });
+
+  if (!isValid) {
+    return res.send({ message: "Not from QStash" });
+  }
+
+  return res.send({ message: "Successfully got ticket" });
 };
 
 export default generateTicketController;
