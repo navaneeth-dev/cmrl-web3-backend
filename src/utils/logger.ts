7e84a2b8e62b230593b481845bb15029232b030e
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-29 10:43:53 +0530

    ignore

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,7 +2,13 @@ import pino from "pino";
 
 const logger = pino(
   process.env.NODE_ENV !== "production"
-    ? { transport: { target: "pino-pretty" } }
+    ? {
+        level: "debug",
+        transport: {
+          target: "pino-pretty",
+          options: { ignore: "pid,hostname,req,res" },
+        },
+      }
     : {}
 );
 export default logger;
