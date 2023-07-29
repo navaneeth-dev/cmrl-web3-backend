f279a55d6e7583e4b357ae01b9240b26ea3c0395
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-29 11:45:51 +0530

    added production to env

--- src/schemas/env.ts
+++ src/schemas/env.ts
@@ -10,7 +10,7 @@ const envSchema = z.object({
   QSTASH_TOKEN: z.string(),
   QSTASH_CURRENT_SIGNING_KEY: z.string(),
   QSTASH_NEXT_SIGNING_KEY: z.string(),
-  NODE_ENV: z.enum(["development"]).default("development"),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
   BITCART_URL: z.string().url(),
 });
 
