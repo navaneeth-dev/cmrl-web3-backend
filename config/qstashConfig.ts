a958526fd2b8f8360c73ca24ad2655c3a3236980
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-30 08:09:44 +0530

    added bullmqconfig

--- config/qstashConfig.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Receiver } from "@upstash/qstash/nodejs";
-import ENV from "../src/schemas/env";
-
-export const client = new Client({ token: ENV?.QSTASH_TOKEN! });
-export const receiver = new Receiver({
-  currentSigningKey: ENV?.QSTASH_CURRENT_SIGNING_KEY!,
-  nextSigningKey: ENV?.QSTASH_NEXT_SIGNING_KEY!,
-});
