88c24646d01fd503190adb52afc90731dc936ba6
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-30 08:28:20 +0530

    remove generateTicketController

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
 import { Express } from "express";
 import healthController from "./controllers/healthController";
 import ticketController from "./controllers/checkTicket";
-import generateTicketController from "./controllers/generateTicketController";
 
 const routes = (app: Express) => {
   app.get("/api/health", healthController);
   app.post("/api/checkTicket", ticketController);
-  app.post("/api/generateTicket", generateTicketController);
 };
 
 export default routes;
