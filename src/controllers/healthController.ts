803f5c818b2e537443081d49d8a14bfcb8252066
Navaneeth Rao <navaneeth@inflorencers.com>
2023-08-11 10:16:40 +0530

    remove node SHIT

--- src/controllers/healthController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-const healthController = (req: Request, res: Response) => {
-  res.send({ message: "Success" });
-};
-
-export default healthController;
