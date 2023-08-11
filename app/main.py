8cb002140640743a942b5c9f5f07497cde719d74
Navaneeth Rao <navaneeth@inflorencers.com>
2023-08-11 19:20:53 +0530

    installed playwright

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+import time
+from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
 
 
-@app.post("/")
-async def root():
+def process():
+    print("Called")
+    time.sleep(10)
+    print("Ended")
+
+
+@app.post("/api/checkTicket")
+async def root(background_tasks: BackgroundTasks):
+    background_tasks.add_task(process)
     return {"message": "Hello World"}
