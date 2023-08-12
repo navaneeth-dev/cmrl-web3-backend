0539e5e1e0c2e2fefe158eb1b678c2535b65af49
Navaneeth Rao <navaneeth@inflorencers.com>
2023-08-12 09:16:35 +0530

    get id

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 import os
 from dotenv import load_dotenv
 from fastapi import BackgroundTasks, FastAPI
-from app import get_ticket
+from pydantic import BaseModel
+from app.get_ticket import get_ticket
 
 
 load_dotenv()
 app = FastAPI()
 
 
+class Ticket(BaseModel):
+    id: str
+
+
 @app.post("/api/checkTicket")
-async def root(background_tasks: BackgroundTasks):
+async def checkTicket(ticket: Ticket, background_tasks: BackgroundTasks):
+    print(ticket.id)
     background_tasks.add_task(get_ticket)
     return {"message": "Hello World"}
